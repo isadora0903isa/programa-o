1b719ddd0fdf4caeafc579914f9911485f961ae9
Isa <104452638+isadora0903isa@users.noreply.github.com>
2024-06-13 11:19:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+const perguntas = [
+    {
+    enunciado: "QUAL O SEU ESPORTE PREFIRIDO?",
+    alternativas: [
+    "MOUNTAIN BIKE",
+    "SKATE"
+    ]
+    },
+    {
+    enunciado: "VOCÊ PREFERE PRATICAR ESPORTES AO AR LIVRE OU EM AMBIENTES FECHADOS?",
+    alternativas: [
+    "AO AR LIVRE",
+    "EM AMBIENTES FECHADOS"
+    ]
+    },
+    {
+    enunciado: "QUAL DOS SEGUINTES ESTILOS VOCÊ PREFERE PARA SE EXERCITAR?",
+    alternativas: [
+    "PARKOUR",
+    "ESPORTES DE AÇÃO"
+    ]
+    },
+    {
+    enunciado: "VOCÊ COSTUMA PRATICAR ESPORTES SOZINHO OU EM GRUPO?",
+    alternativas: [
+    "SOZINHO",
+    "EM GRUPO"
+    ]
+    
+    },
+    {
+    enunciado: "QUAL É O FATOR MAIS IMPORTANTE PARA VOCE AO ESCOLHER UM ESPORTE?",
+    alternativas: [
+    "DESAFIO FÍSICO E ADRENALINA",
+    "SOCIALIZAÇÃO E DIVERSÃO"
+    ]
     }
-}
-
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
-
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0){
-        return [dias,horas,minutos,segundos];
+    ];
+    let atual = 0;
+    let perguntaAtual;
+    const caixaPerguntas = document.getElementById('caixaPerguntas');
+    const alternativasContainer = document.getElementById('alternativas');
+    function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    alternativasContainer.innerHTML = '';
+    perguntaAtual.alternativas.forEach((alternativa, index) => {
+    const button = document.createElement('button');
+    button.textContent = alternativa;
+    button.addEventListener('click', () => {
+    atual++;
+    if (atual < perguntas.length) {
+    mostraPergunta();
     } else {
-        return [0,0,0,0];
+    caixaPerguntas.textContent = 'Você completou o questionário!';
+    alternativasContainer.innerHTML = '';
     }
-}
-
-function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-    for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
+    });
+    alternativasContainer.appendChild(button);
+    });
     }
-}
-
-function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
-}
-
-comecaCronometro();
+    mostraPergunta();
